4cef5da69657735648b2d45b1fd7b25a72ae9229
Uncarbon <75737767+uncarbon97@users.noreply.github.com>
2022-10-15 20:34:54 +0800

    feat: 支持对象存储

---
+++ src/api/oss/OssFileInfoApi.ts
@@ -0,0 +1,59 @@
+import { defHttp } from '/@/utils/http/axios';
+import { OssFileInfoApiResult } from './model/OssFileInfoModel';
+import { getToken } from '/@/utils/auth';
+import { getAppEnvConfig } from '/@/utils/env';
+
+enum Api {
+  REST = '/api/v1/oss/file/infos',
+}
+
+/**
+ * 上传文件信息-分页列表
+ */
+export const listOssFileInfoApi = (queryForm: any) => {
+  // 选择的是日期，补全时分秒部分
+  if (queryForm.beginAt) {
+    queryForm.beginAt += ' 00:00:00';
+  }
+
+  if (queryForm.endAt) {
+    queryForm.endAt += ' 23:59:59';
+  }
+
+  return defHttp.get<OssFileInfoApiResult[]>({
+    url: Api.REST,
+    params: queryForm,
+  });
+};
+
+/**
+ * 上传文件信息-详情
+ */
+export const retrieveOssFileInfoApi = (id: string) => {
+  return defHttp.get<OssFileInfoApiResult>({
+    url: `${Api.REST}/${id}`,
+  });
+};
+
+/**
+ * 上传文件信息-删除
+ */
+export const deleteOssFileInfoApi = (ids: string[]) => {
+  return defHttp.delete<void>({
+    url: Api.REST,
+    params: {
+      ids: ids,
+    },
+  });
+};
+
+/**
+ * 上传文件信息-下载文件
+ */
+export const downloadOssFileApi = (id: string) => {
+  // 从缓存中取出 token；从环境变量中取出全局 BASE_URL
+  const Authorization = getToken();
+  const { VITE_GLOB_UPLOAD_URL } = getAppEnvConfig();
+  // 拼串在新窗口打开
+  window.open(`${VITE_GLOB_UPLOAD_URL}/${id}?Authorization=${Authorization}`);
+};
---
+++ src/api/oss/model/OssFileInfoModel.ts
@@ -0,0 +1,86 @@
+/**
+ * @model 上传文件信息
+ */
+
+/*
+ ---- REQUEST ----
+ */
+
+/**
+ * 上传文件信息-新增/编辑请求体
+ */
+export interface OssFileInfoInsertOrUpdateForm {
+  /**
+   * 存储平台
+   */
+  storagePlatform: string;
+
+  /**
+   * 基础存储路径
+   */
+  storageBasePath: string;
+
+  /**
+   * 存储路径
+   */
+  storagePath: string;
+
+  /**
+   * 存储文件名
+   */
+  storageFilename: string;
+
+  /**
+   * 原始文件名
+   */
+  originalFilename: string;
+
+  /**
+   * 扩展名
+   */
+  extendName: string;
+
+  /**
+   * 文件大小
+   */
+  fileSize: string;
+
+  /**
+   * MD5
+   */
+  md5: string;
+
+  /**
+   * 文件类别
+   */
+  classified: string;
+
+  /**
+   * 云存储直链
+   */
+  directUrl: string;
+}
+
+/*
+ ---- RESPONSE ----
+ */
+
+/**
+ * 上传文件信息-通用响应体
+ */
+export type OssFileInfoApiResult = OssFileInfoInsertOrUpdateForm & {
+  /**
+   * 主键ID
+   */
+  id: string;
+
+  /**
+   * 创建时刻
+   */
+  createdAt: string;
+
+  /**
+   * 更新时刻
+   */
+  updatedAt: string;
+};
